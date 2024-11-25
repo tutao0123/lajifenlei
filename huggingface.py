ca3e14120fed0937cc0702dcb78c96cab43af177
wentaotao <wentaotao@zhejianglab.com>
2024-11-25 17:16:46 +0800

    修改了import

---
+++ huggingface.py
@@ -0,0 +1,18 @@
+from transformers import ViTImageProcessor, ViTForImageClassification, pipeline
+from PIL import Image
+import requests
+
+url = 'http://images.cocodataset.org/val2017/000000039769.jpg'
+image = Image.open(requests.get(url, stream=True).raw)
+
+processor = ViTImageProcessor.from_pretrained('google/vit-base-patch16-224')
+pipe = pipeline("image-classification", model="microsoft/resnet-50")
+model = ViTForImageClassification.from_pretrained('google/vit-base-patch16-224')
+
+inputs = processor(images=image, return_tensors="pt")
+outputs = model(**inputs)
+logits = outputs.logits
+# model predicts one of the 1000 ImageNet classes
+predicted_class_idx = logits.argmax(-1).item()
+print("Predicted class:", model.config.id2label[predicted_class_idx])
+
